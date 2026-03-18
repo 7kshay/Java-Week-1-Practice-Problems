d5c63dc959907277d6e217b30f3c151d84b77096
7kshay <aa2149@srmist.edu.in>
2026-03-18 09:21:20 +0530

    Add Problem 1: Calculate Harry's age

---
+++ Level1/HarryAge.java
@@ -0,0 +1,15 @@
+public class HarryAge {
+    public static void main(String[] args) {
+
+        // Fixed values stored as variables (no hard coding)
+        String name = "Harry";
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Result stored as a variable
+        int age = currentYear - birthYear;
+
+        // Output
+        System.out.println(name + "'s age in " + currentYear + " is " + age);
+    }
+}
