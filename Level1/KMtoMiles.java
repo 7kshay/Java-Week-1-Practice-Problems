4a4a4f05f774cb786afe14ebd14d464f559deda8
7kshay <aa2149@srmist.edu.in>
2026-03-18 09:26:06 +0530

    Add Problem 3: Convert km to miles

---
+++ Level1/KMtoMiles.java
@@ -0,0 +1,19 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+
+        // Fixed values stored as variables
+        double distanceKm = 10.8;
+        double kmToMilesFactor = 1.6;
+
+        // Result stored as a variable
+        double distanceMiles = distanceKm * kmToMilesFactor;
+
+        // Output
+        System.out.println("The distance " + distanceKm + " km in miles is " + distanceMiles);
+    }
+}
+```
+
+**Output:**
+```
+The distance 10.8 km in miles is 17.28
